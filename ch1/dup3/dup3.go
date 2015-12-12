a18bc6e8908a5273e22c9b7e1926d7a2db8d21ef
mukulrawat1986 <mukulsrawat@gmail.com>
2015-12-12 10:45:23 +0530

    finished dup3

---
+++ ch1/dup3/dup3.go
@@ -0,0 +1,34 @@
+// Dup3 prints the count and text of lines that appear more than once
+// in the input. It reads the entire input into memory in one big gulp,
+// split it into lines all at once, and then process the lines.
+
+// It only reads names files, not the standard input.
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
